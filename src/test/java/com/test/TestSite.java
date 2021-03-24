f76a89f7543ffc082658565d85a28f2aac9d38c2
pasu <pasu_pathi@outlook.com>
2021-03-24 14:29:53 -0500

    SAMPLE

---
+++ src/test/java/com/test/TestSite.java
@@ -0,0 +1,18 @@
+package com.test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class TestSite {
+
+	@Test
+	public void testSite() {
+		
+	}
+
+}
