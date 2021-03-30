4536313b4a2b57551c506af285beb9309c4ef6ed
pasu <pasu_pathi@outlook.com>
2021-03-30 15:55:02 -0500

    java version

--- src/test/java/com/test/TestSite.java
+++ src/test/java/com/test/TestSite.java
@@ -2,7 +2,10 @@ package com.test;
 
 import java.util.concurrent.TimeUnit;
 
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -10,9 +13,13 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class TestSite {
 
+	@BeforeClass
+	public void pop() {
+		
+	}
+	
 	@Test
 	public void testSite() {
-		
 	}
 
 }
